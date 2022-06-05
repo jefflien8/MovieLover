f5dae96a185808fb0864420774b0eeddf83fed9c
jefflien8 <jefflien8@gmail.com>
2022-06-05 10:07:03 +0800

    0605

--- crawler/Ambassador.py
+++ crawler/Ambassador.py
@@ -11,7 +11,7 @@ engine = create_engine('mysql+pymysql://root:12345678@localhost/movielover')
 host='localhost'
 port=3306
 user='root'
-passwd='123456'
+passwd='12345678'
 database='movielover'
 
 conn=pymysql.connect(
